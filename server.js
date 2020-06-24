e8851ee3893a99e1f0893202740c3bf0f248ac3f
jherd801 <jherd801@gmail.com>
2020-06-23 18:17:06 -0600

    Got the server to listen on port 3000

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./userModel.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -15,16 +14,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
-
-app.post("/submit", ({ body }, res) => {
-  User.create(body)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
